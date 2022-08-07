3cf787df26e33aa1cefd9acdb741fa0b07dfb256
Hesbon Osoro <hesbonosoro1@gmail.com>
2022-08-07 16:49:07 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+	let faceio;
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	useEffect(() => {
+		faceio = new faceIO('fioa4303');
+	}, []);
+
+	const handleSignIn = async () => {
+		try {
+			let response = await faceio.enroll({
+				locale: 'auto',
+				payload: {
+					email: 'example@example.com',
+					pin: '12345',
+				},
+			});
+			console.log(`Unique Facial ID: ${response.facialId}
+			Enrollment Date: ${response.timestamp}
+			Gender: ${reponse.details.gender}
+			Age Approximation: ${reponse.details.age}`);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+
+	const handleLogin = async () => {
+		try {
+			let response = await faceio.authenticate({ locale: 'auto' });
+			console.log(`Unique Facial ID: ${response.facialId}
+			Payload: ${response.payload}
+			`);
+		} catch (err) {
+			console.log(err);
+		}
+	};
+	return (
+		<section>
+			<h1>Face Authentication by FaceIO</h1>
+			<button onClick={handleSignIn}>Sign-in</button>
+			<button onClick={handleLogin}>Log-in</button>
+		</section>
+	);
 }
 
-export default App
+export default App;
